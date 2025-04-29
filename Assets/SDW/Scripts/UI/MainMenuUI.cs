e76ce8cab75df4e048a56681659d51ad5782dbc3
Rio_Song <rio.dw.song@gmail.com>
2025-04-29 10:39:32 +0900

    [Fix] New Game 버튼 관련 버그 수정
    New Game 버튼 클릭 시 팝업창이 뜨지 않고 다음 씬으로 전환되는 버그 해결

--- Assets/SDW/Scripts/UI/MainMenuUI.cs
+++ Assets/SDW/Scripts/UI/MainMenuUI.cs
@@ -6,15 +6,11 @@ using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
-    [SerializeField] private Button m_newGameYesButton;
     [SerializeField] private Button m_exitButton;
 
-    [Header("New Game Scene Name")]
-    [SerializeField] private string m_sceneName;
     
     void Start()
     {
-        m_newGameYesButton.onClick.AddListener(() => GameManager.Instance.SceneLoader(m_sceneName));
         m_exitButton.onClick.AddListener(GameManager.Instance.Exit);
     }
 }
