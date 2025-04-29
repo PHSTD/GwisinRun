8487389fd551e441018c1e6881f0f9ba7504c8c5
Rio_Song <rio.dw.song@gmail.com>
2025-04-30 00:08:25 +0900

    [Feat] GameStart, GameOver, GameRestart, GameClear 구현 [1/2]

--- Assets/SDW/Scripts/Manager/GameManager.cs
+++ Assets/SDW/Scripts/Manager/GameManager.cs
@@ -17,9 +17,9 @@ public class GameManager : MonoBehaviour
     //todo 추후 리팩토링 시 검토
     // public UIManager UI;
 
-    //# 게임 Time Attack에 사용할 Event
-    [NonSerialized] public UnityEvent OnGameStart;
-    [NonSerialized] public UnityEvent OnGameOver;
+    //todo 추후 사용 고민 게임 Time Attack에 사용할 Event
+    // [NonSerialized] public UnityEvent OnGameStart;
+    // [NonSerialized] public UnityEvent OnGameOver;
     [NonSerialized] public UnityEvent OnTimeChanged;
 
     //# 게임 Time Attack에 사용할 필드와 프로퍼티
@@ -28,13 +28,16 @@ public class GameManager : MonoBehaviour
 
     private float m_currentTime;
     public float CurrentTime => m_currentTime;
-
-    private float m_highestTime;
-    public float HighestTime => m_highestTime;
+    private float m_bestTime;
+    public float BestTime => m_bestTime;
 
     //# GameOver 관련 필드와 프로퍼티
     private bool m_isGameOver;
     public bool IsGameOver => m_isGameOver;
+    private bool m_isPaused;
+    public bool IsPaused => m_isPaused;
+    private bool m_isCleared;
+    public bool IsCleared => m_isCleared;
 
 
     public static void CreateInstance()
@@ -59,31 +62,30 @@ public class GameManager : MonoBehaviour
     private void OnEnable()
     {
         //# 현재는 GameOver만 등록
-        OnGameOver = new UnityEvent();
-        OnGameOver.AddListener(GameOver);
-        OnGameStart = new UnityEvent();
+        // OnGameOver = new UnityEvent();
+        // OnGameOver.AddListener(GameOver);
+        // OnGameStart = new UnityEvent();
         OnTimeChanged = new UnityEvent();
     }
 
     private void OnDisable()
     {
-        OnGameStart.RemoveAllListeners();
-        OnGameOver.RemoveAllListeners();
+        // OnGameStart.RemoveAllListeners();
+        // OnGameOver.RemoveAllListeners();
         OnTimeChanged.RemoveAllListeners();
     }
 
     private void Update()
     {
-        //# 타입어택 적용 시 사용할 부분, 현재 주석 처리
-        // if (!m_isGameOver)
-        // {
-        //     m_currentTime += Time.deltaTime;
-        //     if (m_currentTime - m_previousTime >= 0.1f)
-        //     {
-        //         OnTimeChanged?.Invoke();
-        //         m_previousTime = m_currentTime;
-        //     }
-        // }
+        if (!m_isGameOver && !m_isPaused && !m_isCleared)
+        {
+            m_currentTime += Time.deltaTime;
+            if (m_currentTime - m_previousTime >= 0.1f)
+            {
+                OnTimeChanged?.Invoke();
+                m_previousTime = m_currentTime;
+            }
+        }
     }
 
     public void GameStart()
@@ -91,16 +93,35 @@ public class GameManager : MonoBehaviour
         m_currentTime = 0;
         m_previousTime = 0;
         m_isGameOver = false;
-        OnGameStart?.Invoke();
+        // OnGameStart?.Invoke();
     }
 
+    //todo GameOver는 Level에서 호출
     private void GameOver()
     {
-        if (m_currentTime > m_highestTime)
-        {
-            m_highestTime = m_currentTime;
-        }
         m_isGameOver = true;
+    }
+
+    //todo GameClear는 Level에서 호출
+    public void GameClear(string sceneName)
+    {
+        if (m_currentTime < m_bestTime)
+        {
+            //todo PlayerPrefs 에 best score를 맵별로 저장해서 활용?
+            m_bestTime = m_currentTime;
+            
+            //# Write
+            PlayerPrefs.SetFloat(sceneName, m_bestTime);
+        }
+    }
+    
+    //todo GameRestart는 Level에서 호출
+    public void GameRestart(string sceneName)
+    { 
+        //# Read
+        m_bestTime= PlayerPrefs.GetFloat(sceneName);
+        
+        GameStart();
     }
 
     public void SceneLoader(string sceneName)
