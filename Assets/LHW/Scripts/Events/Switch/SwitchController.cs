42b82c7e77e10c0e18cf318e1c777438946fe1a2
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-04-29 14:47:24 +0900

    [Feat]SwitchController 기능 개편
    SwitchController의 기능을 개편하여, animator를 리스트로 받고 2개 이상의 철창 문이 있을 때 번갈아가면서 바뀌도록 변경

--- Assets/LHW/Scripts/Events/Switch/SwitchController.cs
+++ Assets/LHW/Scripts/Events/Switch/SwitchController.cs
@@ -1,8 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SwitchController : MonoBehaviour, IInteractable
 {
-    [SerializeField] Animator m_cageAnimator;
+    [SerializeField] List<Animator> m_objectAnimator;
     private bool m_switchOn = false;
     private bool m_interactable = false;
 
@@ -20,7 +21,22 @@ public class SwitchController : MonoBehaviour, IInteractable
     public void Interact()
     {
         if (m_interactable)
-        m_switchOn = !m_switchOn;
-        m_cageAnimator.SetBool("IsOpen", m_switchOn);
+        {
+            m_switchOn = !m_switchOn;
+            for (int i = 0; i < m_objectAnimator.Count; i++)
+            {
+                boolAnimator(m_objectAnimator[i]);
+            }
+        }
     }
+
+    // 현 단계에선 번갈아가면서 bool값이 번갈아가게 되어 있으나,
+    // bool 변수 세부 값을 조정할 수 있도록 세팅할 건지도 고민중
+    private void boolAnimator(Animator animator)
+    {
+        m_switchOn = !m_switchOn;
+        animator.SetBool("IsOpen", m_switchOn);
+    }
+      
+
 }
