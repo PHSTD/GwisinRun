059ef2809372e9d654d1cae68b7a76466eaadb24
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-04-29 15:17:32 +0900

    [feat]ItemInfoUIController 기능 개편
    플레이어가 아이템과 일정 거리 내에 있을 때에만 아이템 정보 UI가 출력되도록 기능 수정

--- Assets/LHW/Scripts/UI/ItemInfoUIController/ItemInfoUIController.cs
+++ Assets/LHW/Scripts/UI/ItemInfoUIController/ItemInfoUIController.cs
@@ -5,6 +5,8 @@ public class ItemInfoUIController : MonoBehaviour
     [SerializeField] Canvas m_canvas;
     [SerializeField] GameObject m_panel;
     private RectTransform m_panelpos;
+    [SerializeField] private float m_detectRadius;
+    [SerializeField] private LayerMask m_playerLayer;
 
     public void Awake()
     {
@@ -14,15 +16,22 @@ public class ItemInfoUIController : MonoBehaviour
 
     private void OnMouseOver()
     {
-        m_panel.SetActive(true);
+        if (Physics.OverlapSphere(transform.position, m_detectRadius, m_playerLayer).Length > 0)
+        {
+            m_panel.SetActive(true);
 
-        Vector2 localPos = Vector2.zero;
+            Vector2 localPos = Vector2.zero;
 
-        RectTransform rectTransform = m_canvas.transform as RectTransform;
-        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+            RectTransform rectTransform = m_canvas.transform as RectTransform;
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
 
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, mousePos, m_canvas.worldCamera, out localPos);
-        m_panelpos.anchoredPosition = localPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, mousePos, m_canvas.worldCamera, out localPos);
+            m_panelpos.anchoredPosition = localPos;
+        }
+        else
+        {
+            m_panel.SetActive(false);
+        }
     }
 
     private void OnMouseExit()
