f79c43d00db2b29bbe51463f2703b00b8da71ee5
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-05-02 02:03:11 +0900

    [Chores]SwitchController를 플레이어와 상호작용 가능하게 기능 수정

--- Assets/LHW/Scripts/Events/Switch/SwitchController.cs
+++ Assets/LHW/Scripts/Events/Switch/SwitchController.cs
@@ -4,34 +4,19 @@ using UnityEngine;
 public class SwitchController : MonoBehaviour, IInteractable
 {
     [SerializeField] List<Animator> m_objectAnimator;
-    private bool m_switchOn = false;
-    private bool m_interactable = false;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-            m_interactable = true;
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-            m_interactable = false;
-    }
 
     public void Interact()
     {
-        if (m_interactable)
+        for (int i = 0; i < m_objectAnimator.Count; i++)
         {
-            for (int i = 0; i < m_objectAnimator.Count; i++)
-            {
-                boolAnimator(m_objectAnimator[i]);
-            }
-        }        
+            boolAnimator(m_objectAnimator[i]);
+        }
     }
 
     private void boolAnimator(Animator animator)
     {
-        m_switchOn = !m_switchOn;
-        animator.SetBool("IsOpen", m_switchOn);
+        bool isOpen = animator.GetBool("IsOpen");
+        isOpen = !isOpen;
+        animator.SetBool("IsOpen", isOpen);
     }
 }
