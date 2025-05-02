395f9456d2e7953a479749105ccb0bc435ff4bb3
PHSTD <c.oadib.autoan123@gmail.com>
2025-05-02 20:56:30 +0900

    [Feat] 스태미너 % 계산해서 느려지는 기능 추가

--- Assets/JGH/Script/Player/PlayerHealth.cs
+++ Assets/JGH/Script/Player/PlayerHealth.cs
@@ -33,6 +33,15 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         }
     }
 
+    private void Update()
+    {
+        Debug.ClearDeveloperConsole(); 
+        Debug.Log($"MaxH{MaxHealth}");
+        Debug.Log($"CurrH{CurrentHealth}");
+        Debug.Log($"MaxS{MaxStamina}");
+        Debug.Log($"CurrS{CurrentStamina}");
+    }
+
     // TODO: 게임 오버 씬으로 전환 필요
     public static void Die()
     {
@@ -42,6 +51,7 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     
     public static void StaminaPlus()
     {
+        // 일정 시간 간격으로 스태미너 증가 처리 (0.05초마다 1씩 증가)
         m_timer += Time.deltaTime;
         if (m_timer >= 0.05f)
         {
@@ -53,28 +63,39 @@ public class PlayerHealth : MonoBehaviour, IDamageable
 
     public static void StaminaMinus()
     {
+        // 일정 시간 간격으로 스태미너 감소 처리 (0.05초마다 1씩 감소)
         m_timer += Time.deltaTime;
         if (m_timer >= 0.05f)
         {
-            //todo 스태미너 100일 경우 -> 예를 들어 1이 이깍히면 1%(맥스)
-            /*
-             * todo runSpeed의 minimum : walkspeed
-             * runSpeed의 1%(맥스)
-            */
-            // if ()
-            // {
-                
-            // }
-            // else
-            // {
-                CurrentStamina--;
-                m_timer = 0f;
-            // }
+            CurrentStamina--;
+            m_timer = 0f;
         }
+
+        // 스태미너가 0 이하일 경우 걷는 속도로 제한
         if (CurrentStamina <= 0)
         {
             CurrentStamina = 0;
+
+            // 완전히 탈진하면 강제로 걷기 속도로 고정
             PlayerMove.m_speed = PlayerMove.m_walkSpeed;
+            return;
+        }
+
+        // 현재 스태미너 비율 
+        float staminaPercent = (float)CurrentStamina / MaxStamina;
+
+        // 스태미너가 20% 이하일 경우에만 속도 보간 적용
+        if (staminaPercent <= 0.3f)
+        {
+            // 0% ~ 20% 구간
+            float t = staminaPercent / 0.3f;
+
+            // 걷기 속도와 달리기 속도 사이를 보간
+            // t가 1이면 runSpeed, t가 0이면 walkSpeed
+            float targetSpeed = Mathf.Lerp(PlayerMove.m_walkSpeed, 10f, t); // 10f는 달리기 속도
+
+            // 보간된 속도를 적용
+            PlayerMove.m_speed = targetSpeed;
         }
     }
 }
