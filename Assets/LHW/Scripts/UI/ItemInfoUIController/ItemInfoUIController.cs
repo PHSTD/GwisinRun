2ef131483857476cb7232edb52e64cc9a0746b19
LeeHyungwon-4dimonk <dlguddnjs1998@gmail.com>
2025-05-02 15:09:48 +0900

    [Feat]CrossHair(중심점 출력) 기능 추가
    CrossHair 기능 구현(다만 씬에는 반영하지 않음, 코드만 작성 후 테스트 진행)

--- Assets/LHW/Scripts/UI/ItemInfoUIController/ItemInfoUIController.cs
+++ Assets/LHW/Scripts/UI/ItemInfoUIController/ItemInfoUIController.cs
@@ -1,10 +1,6 @@
-using System;
 using System.Collections;
-using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
+using UnityEngine;
 
 public class ItemInfoUIController : MonoBehaviour
 {
@@ -14,7 +10,7 @@ public class ItemInfoUIController : MonoBehaviour
     private RectTransform m_panelpos;
     [SerializeField] private float m_detectRadius;
     [SerializeField] private LayerMask m_playerLayer;
-    
+
     private Coroutine m_refreshCoroutine;
     private bool m_canOn;
 
@@ -28,44 +24,51 @@ public class ItemInfoUIController : MonoBehaviour
         var interactionKey = PlayerPrefs.GetString("Interaction");
         if (string.IsNullOrEmpty(interactionKey))
             return;
-        
+
         if (gameObject.CompareTag("Item"))
         {
-            m_popupText.text= $"아이템을 얻으려면 [{interactionKey}]를 누르세요.";
+            m_popupText.text = $"아이템을 얻으려면 [{interactionKey}]를 누르세요.";
         }
-        
+
         else if (gameObject.CompareTag("InteractableObject"))
         {
-            m_popupText.text= $"상호작용을 하려면 [{interactionKey}]를 누르세요.";
+            m_popupText.text = $"상호작용을 하려면 [{interactionKey}]를 누르세요.";
         }
     }
 
 
     private void OnMouseOver()
     {
-        m_canOn = true;
-        if (m_refreshCoroutine != null)
-            return;
-        
-        if (Physics.OverlapSphere(transform.position, m_detectRadius, m_playerLayer).Length > 0)
+        if (GameManager.Instance.IsPaused)
         {
-            m_panel.SetActive(true);
-            UpdateTextMessage();
-
-            Vector3 localPos = Vector3.zero;
-
-            RectTransform rectTransform = m_canvasRectTransfom.transform as RectTransform;
-            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
-            
-
-            RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, mousePos, m_canvasRectTransfom.worldCamera, out localPos);
-            m_panelpos.anchoredPosition = localPos;
-
-            m_refreshCoroutine = StartCoroutine(RefreshTime());
+            m_panel.SetActive(false);
         }
         else
         {
-            m_panel.SetActive(false);
+            m_canOn = true;
+            if (m_refreshCoroutine != null)
+                return;
+
+            if (Physics.OverlapSphere(transform.position, m_detectRadius, m_playerLayer).Length > 0)
+            {
+                m_panel.SetActive(true);
+                UpdateTextMessage();
+
+                Vector3 localPos = Vector3.zero;
+
+                RectTransform rectTransform = m_canvasRectTransfom.transform as RectTransform;
+                Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+
+                RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, mousePos, m_canvasRectTransfom.worldCamera, out localPos);
+                m_panelpos.anchoredPosition = localPos;
+
+                m_refreshCoroutine = StartCoroutine(RefreshTime());
+            }
+            else
+            {
+                m_panel.SetActive(false);
+            }
         }
     }
 
