1f4db7be211127833f2a7e94db543c5cc3074204
Rio_Song <rio.dw.song@gmail.com>
2025-05-06 15:29:06 +0900

    [Chore] 조명 그림자 관련 설정 변경, 프리팹 폴더 변경

--- Assets/Hyo/Script/Item/UsableItem.cs
+++ Assets/Hyo/Script/Item/UsableItem.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.tvOS;
 
 [Serializable]
 public class UsableItem : Item, IUsable
